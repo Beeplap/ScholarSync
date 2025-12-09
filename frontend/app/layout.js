af1f55b1be4e9a45768d4489b2fe04f176f82637
beeplap <ghartibeeplap@gmail.com>
2025-12-09 16:00:10 +0545

    refactor: update title and description

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,14 +1,14 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'Attendance Tracker',
-  description: 'Login and role-based dashboards',
-}
+  title: "ScholarSync",
+  description: "Login and role-based dashboards",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="light">
       <body className="antialiased">{children}</body>
     </html>
-  )
+  );
 }
